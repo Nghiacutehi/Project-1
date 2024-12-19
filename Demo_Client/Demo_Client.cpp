da4100c11360eff403e2e0bc424d9f76090956ba
Nghiacutehi <thnghia23@clc.fitus.edu.vn>
2024-12-19 20:50:06 +0700

    Add files via upload

---
+++ Demo_Client/Demo_Client.cpp
@@ -0,0 +1,165 @@
+﻿// Demo_Client.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Demo_Client.h"
+#include "afxsock.h"
+
+#include <csignal>
+#include <thread>
+
+#include <vector>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#define  NUM_CONNECTIONS 4
+#endif
+
+
+// The one and only application object
+
+CWinApp theApp;
+
+using namespace std;
+
+
+
+void signalHandler(int signum) {
+	std::cout << "\nCtrl+C pressed. Program is exiting\n";
+	exit(0); 
+}
+
+
+
+
+
+
+void download_part(int part_num, CSocket& client, int file_size) {
+
+	/*
+	int part_size = file_size / NUM_CONNECTIONS;
+
+	// Đặt vị trí con trỏ file sao cho mỗi phần nhận được là khác nhau
+	int start_byte = part_num * part_size;
+	client.Send(&start_byte, sizeof(start_byte), 0); // Gửi vị trí bắt đầu cho server
+
+	// Nhận phần dữ liệu từ server
+	char* buffer = new char[part_size];
+	int bytesReceived = client.Receive(buffer, part_size, 0);
+
+	// Đảm bảo dữ liệu nhận được không bị lỗi
+	if (bytesReceived > 0) {
+		printf("Part %d received, %d bytes\n", part_num, bytesReceived);
+		// Xử lý dữ liệu nhận được (ví dụ in ra màn hình)
+		cout.write(buffer, bytesReceived);
+	}
+	else {
+		printf("Error receiving part %d\n", part_num);
+	}
+
+	delete[] buffer;
+
+	*/
+
+	int num;
+	client.Receive(&num, sizeof(num), 0);
+
+	cout << num << endl;
+
+
+}
+
+
+
+
+
+
+
+int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
+{
+	cout << "Program is running. Press Ctrl+C to stop...\n";
+	signal(SIGINT, signalHandler);
+
+	int nRetCode = 0;
+	while (true)
+	{
+		HMODULE hModule = ::GetModuleHandle(NULL);
+
+		if (hModule != NULL)
+		{
+
+			if (!AfxWinInit(hModule, NULL, ::GetCommandLine(), 0))
+			{
+
+				_tprintf(_T("Fatal Error: MFC initialization failed\n"));
+				nRetCode = 1;
+			}
+			else
+			{
+
+				AfxSocketInit(NULL);
+				CSocket client;
+
+				client.Create();
+				client.Connect(_T("127.0.0.1"), 4567);
+
+
+
+				int number_continue = 0;
+
+				int count = 0;
+
+
+
+
+				char* buffer = nullptr;
+
+				int file_size = 0;
+				client.Receive(&file_size, sizeof(file_size), 0);
+
+				buffer = new char[file_size + 1];
+
+				int bytesReceived;
+
+				bytesReceived = client.Receive((char*)buffer, file_size, 0);
+
+				buffer[file_size] = '\0';
+
+				printf("Cac file co tren Server: \n\n%s\n", buffer);
+
+				delete(buffer);
+
+				client.Receive(&file_size, sizeof(file_size), 0); // Nhận kích thước file từ server
+
+				// Mở 4 kết nối song song để tải các phần của file
+				vector<thread> download_threads;
+
+				// Tạo 4 thread để tải các phần của file
+				for (int i = 0; i < NUM_CONNECTIONS; i++) {
+					download_threads.push_back(thread(download_part, i, ref(client), file_size));
+				}
+
+				// Chờ cho tất cả các thread tải xong
+				for (auto& t : download_threads) {
+					t.join();
+				}
+
+			}
+
+
+
+			cin.ignore();
+
+		
+		}
+		else
+		{
+			// TODO: change error code to suit your needs
+			_tprintf(_T("Fatal Error: GetModuleHandle failed\n"));
+			nRetCode = 1;
+		}
+
+	}
+	return nRetCode;
+
+}
---
+++ Demo_Server/Demo_Server.cpp
@@ -0,0 +1,221 @@
+﻿// Demo_Server.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Demo_Server.h"
+#include "afxsock.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#define BUFFER_SIZE 1024
+#endif
+
+
+
+CWinApp theApp;
+
+using namespace std;
+
+DWORD WINAPI list_file(LPVOID arg)
+{
+	SOCKET* hConnected = (SOCKET*)arg;
+	CSocket mysock;
+	mysock.Attach(*hConnected);
+
+
+
+
+	FILE* list_file;
+	char* buffer = nullptr;
+	long file_size;
+
+	list_file = fopen("ListFile.txt", "rb");
+	if (list_file == NULL) {
+		perror("Unable to open file");
+		return 1;
+	}
+
+	size_t bytesRead;
+
+	fseek(list_file, 0, SEEK_END);
+	file_size = ftell(list_file);
+	rewind(list_file);
+
+	buffer = (char*)malloc(file_size + 1);
+	if (buffer == NULL) {
+		perror("Unable to allocate memory for buffer");
+		fclose(list_file);
+		return 1;
+	}
+
+	size_t bytes_read = fread(buffer, 1, file_size, list_file);
+	if (bytes_read != file_size) {
+		printf("Error reading the file. Expected %ld bytes but read %zu bytes.\n", file_size, bytes_read);
+		free(buffer);
+		fclose(list_file);
+		return 1;
+	}
+
+	mysock.Send(&bytes_read, sizeof(bytes_read), 0);
+
+	int result = mysock.Send(buffer, bytes_read, 0);
+	if (result == SOCKET_ERROR) {
+		printf("Error sending data: %d\n", WSAGetLastError());
+	}
+
+	buffer[bytes_read] = '\0';
+	printf("Cac file co tren Server: \n\n%s\n", buffer);
+	
+
+
+	free(buffer);
+	fclose(list_file);
+
+
+	delete hConnected;
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+DWORD WINAPI four_chunks(LPVOID arg)
+{
+	SOCKET* hConnected = (SOCKET*)arg;
+	CSocket mysock;
+	mysock.Attach(*hConnected);
+
+
+	/*
+	FILE* file;
+	char* buffer = nullptr;
+	long file_size;
+
+	file = fopen("100MB.bin", "rb");
+	if (file == NULL) {
+		perror("Không thể mở file");
+		return 1;
+	}
+
+	fseek(file, 0, SEEK_END);
+	file_size = ftell(file);
+	rewind(file);
+
+	long chunk_size = file_size / 4;
+
+	size_t bytesRead;
+	buffer = (char*)malloc(BUFFER_SIZE);
+	if (buffer == NULL) {
+		perror("Không thể cấp phát bộ nhớ cho buffer");
+		fclose(file);
+		return 1;
+	}
+
+	long position = chunk_size * (intptr_t)arg;
+	fseek(file, position, SEEK_SET);
+
+	int num_parts = chunk_size / BUFFER_SIZE + (chunk_size % BUFFER_SIZE != 0);
+	mysock.Send(&num_parts, sizeof(num_parts), 0);
+
+
+	for (int part = 0; part < num_parts; part++) {
+		bytesRead = fread(buffer, 1, BUFFER_SIZE, file);
+		mysock.Send(&bytesRead, sizeof(bytesRead), 0);
+		mysock.Send(buffer, bytesRead, 0);
+
+		if (bytesRead != chunk_size) {
+			printf("Lỗi khi đọc file. Dự kiến %ld byte nhưng đọc được %zu byte.\n", chunk_size, bytesRead);
+			free(buffer);
+			fclose(file);
+			return 1;
+		}
+	}
+
+
+	printf("Đã gửi phần %ld cho client\n", (intptr_t)arg + 1);
+
+	
+	
+
+	free(buffer);
+	fclose(file);
+
+	*/
+
+	mysock.Send(&(intptr_t)arg, sizeof((intptr_t)arg), 0);
+
+	delete hConnected; // Giải phóng socket
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
+{
+	int nRetCode = 0;
+
+	HMODULE hModule = ::GetModuleHandle(NULL);
+
+	if (hModule != NULL)
+	{
+		// initialize MFC and print and error on failure
+		if (!AfxWinInit(hModule, NULL, ::GetCommandLine(), 0))
+		{
+			// TODO: change error code to suit your needs
+			_tprintf(_T("Fatal Error: MFC initialization failed\n"));
+			nRetCode = 1;
+		}
+		else
+		{
+			// TODO: code your application's behavior here.
+			AfxSocketInit(NULL);
+			CSocket server, s;
+			DWORD threadID;
+			HANDLE threadStatus;
+
+			server.Create(4567);
+			do {
+				printf("Server lang nghe ket noi tu client\n");
+				server.Listen();
+				server.Accept(s);
+
+				SOCKET* hConnected = new SOCKET();
+
+				*hConnected	= s.Detach();
+				
+				threadStatus = CreateThread(NULL, 0, list_file, hConnected, 0, &threadID);
+				for (int i = 0; i < 4; i++)
+				{
+
+					SOCKET* hConnected = new SOCKET();
+					*hConnected = s.Detach();
+
+					threadStatus = CreateThread(NULL, 0, four_chunks, (LPVOID)(intptr_t)i, 0, &threadID);
+				}
+			} while (1);
+		}
+	}
+	else
+	{
+		// TODO: change error code to suit your needs
+		_tprintf(_T("Fatal Error: GetModuleHandle failed\n"));
+		nRetCode = 1;
+	}
+
+	return nRetCode;
+}
+
+
